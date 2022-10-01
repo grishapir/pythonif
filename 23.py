8a49ae4fab70c548df6a065f14ebc703fe12ac14
grishapir <114808385+grishapir@users.noreply.github.com>
2022-10-01 17:49:06 +0700

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,8 @@
+a=int(input('введите число '))
+b=int(input('введите число '))
+if a<b:
+    print('a<b')
+    print("число а меньше верно")
+else:
+    print('a<b')
+    print("число а больше неверно")
---
+++ 55.py
@@ -0,0 +1,24 @@
+import keyboard
+
+while True:
+    if keyboard.is_pressed('0') == True:
+        print("ты ничтожество")
+    if keyboard.is_pressed('1') == True:
+        print("ты родился")
+    if keyboard.is_pressed('2') == True:
+        print("ты растешь")
+    if keyboard.is_pressed('3') == True:
+        print("тебе 3")
+    if keyboard.is_pressed('4') == True:
+        print("тебе 4 как много")
+    if keyboard.is_pressed('5') == True:
+        print("тебе 5 много осталось чуть чуть")
+    if keyboard.is_pressed('6') == True:
+        print("ты есть что то в этом мире")
+    if keyboard.is_pressed('7') == True:
+        print(" остается 2")
+    if keyboard.is_pressed('8') == True:
+        print("остается 1")
+    if keyboard.is_pressed('9') == True:
+        print("бум")
+    
