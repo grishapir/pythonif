ce871b19652fbf1f15406cbbb35ba90fcfe715b9
grishapir <114808385+grishapir@users.noreply.github.com>
2023-02-18 17:52:41 +0700

    Add files via upload

---
+++ 125.py
@@ -0,0 +1,3 @@
+a = int(input("Введите факториал числа до 9"))
+while a == 1 or a == 2 or a == 3 or a == 4 or a == 5 or a == 6 or a == 7 or a == 8 or a == 9:
+    a = input("Введите факториал числа до 9")
