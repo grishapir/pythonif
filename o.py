4d4cecd4a89a87393421958e0f6b4ff65c00dd93
grishapir <114808385+grishapir@users.noreply.github.com>
2023-04-29 17:59:54 +0700

    Add files via upload

--- o.py
+++ o.py
@@ -1,9 +1,25 @@
-f = open("text1.txt", 'r')
-a = open("text2.txt", 'r')
-text = f.read()
-text2 = a.read()
-s = open("text3.txt", 'w')
-s.write(text + text2)
-s.close()
-f.close()
-a.close()
+from tkinter import *
+
+
+
+def jak():
+    btn.configure(text= ent.get())
+    lbl.configure(text= "Не нажимать!")
+window = Tk()
+window.title("Привет")
+window.geometry('400x400')
+lbl = Label(window, text="Привет", font=("Arial Bold", 50))
+btn = Button(window, text= "Не нажимать!", command=jak)
+ent = Entry(window, width = 10)
+ent.focus()
+newText = ent.get()
+
+
+
+
+
+
+lbl.pack()
+ent.pack()
+btn.pack()
+window.mainloop()
